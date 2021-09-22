3f9ae9589888558c86648fc0e4d365129e383fda
chaitanya7666 <chaitu.y25@gmail.com>
2021-09-22 09:59:41 +0530

    implemented likes functionality

--- src/components/Post2/Post2.jsx
+++ src/components/Post2/Post2.jsx
@@ -25,11 +25,11 @@ import BookmarkIcon from "@material-ui/icons/Bookmark";
 import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
 import { Link } from "react-router-dom";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
-import Snackbar from '@material-ui/core/Snackbar';
-import Alert from '@material-ui/lab/Alert';
-
+import Snackbar from "@material-ui/core/Snackbar";
+import Alert from "@material-ui/lab/Alert";
 
 const Post2 = ({ post }) => {
+  console.log(post.text,post.likedBy)
   const [currentUserDoc, setCurrentUserDoc] = useContext(
     CurrentUserDetailsContext
   );
@@ -40,20 +40,20 @@ const Post2 = ({ post }) => {
   const [like, setLike] = useState(false);
   const [save, setSave] = useState(false);
   const [expanded, setExpanded] = React.useState(false);
-  const [openUrlCopiedSnackBar, setOpenUrlCopiedSnackBar] = React.useState(false);
+  const [openUrlCopiedSnackBar, setOpenUrlCopiedSnackBar] =
+    React.useState(false);
 
   const handleShareButtonSnackBarOpen = () => {
     setOpenUrlCopiedSnackBar(true);
   };
 
   const handleShareButtonSnackBarClose = (event, reason) => {
-    if (reason === 'clickaway') {
+    if (reason === "clickaway") {
       return;
     }
 
     setOpenUrlCopiedSnackBar(false);
   };
-
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
@@ -133,15 +133,119 @@ const Post2 = ({ post }) => {
     });
   };
 
+  const addToLikedByArrayInFireStore = async (docToModify) =>{
+  console.log("docToModify.email",docToModify.email)
+  console.log("docToModify.email",docToModify.posts)
+
+    const currentUserDocRef = doc(db, "users", docToModify.email);
+
+    await updateDoc(currentUserDocRef, {
+      posts: [...docToModify.posts],
+    });
+  }
+
+  const addToLikedByArrayInCurrentUserDoc = () => {
+    setCurrentUserDoc((prevState) => {
+      const currentUserDocCopy = { ...prevState };
+      const postToUpdate = currentUserDocCopy.posts.find(
+        (eachPost) => eachPost.id === post.id
+      );
+      if (postToUpdate.likedBy.indexOf(currentUserDoc.email) === -1) {
+        postToUpdate.likedBy.push(currentUserDocCopy?.email);
+      }
+
+      return { ...currentUserDocCopy };
+    });
+  };
+
+  const addToLikedByArrayInAllUserDocs = () => {
+    setAllUserDocs((prevState) => {
+      const allUserDocsCopy = [...prevState];
+      const docToModify = allUserDocsCopy.find(
+        (doc) => doc.email === post.email
+      );
+      const postToModify = docToModify.posts.find(
+        (eachPost) => eachPost.id === post.id
+      );
+      if (postToModify.likedBy.indexOf(currentUserDoc.email) === -1) {
+        postToModify.likedBy.push(currentUserDoc?.email);
+        addToLikedByArrayInFireStore({...docToModify});
+        // postToModify.likedBy.push("hello sir");
+
+      }
+      return [...allUserDocsCopy];
+    });
+  };
+
   const addToLikedPostsArray = () => {
     let likedPostsArray = [...currentUserDoc?.likedPosts];
     addToLikedPostsArrayInCurrentUserDoc();
     addToLikedPostsArrayInAllUserDocs();
+    if (post.email === currentUserDoc.email) {
+      addToLikedByArrayInCurrentUserDoc();
+    }
+    addToLikedByArrayInAllUserDocs();
+    console.log(post.likedBy);
   };
+
+  const removeFromLikedByArrayInFireStore = async (docToModify) => {
+    console.log("docToModify.email",docToModify.email)
+  console.log("docToModify.email",docToModify.posts)
+
+    const currentUserDocRef = doc(db, "users", docToModify.email);
+
+    await updateDoc(currentUserDocRef, {
+      posts: [...docToModify.posts],
+    });
+  }
+
+  const removeFromLikedByArrayInCurrentUserDoc = () =>{
+    setCurrentUserDoc((prevState) => {
+      const currentUserDocCopy = { ...prevState };
+      const postToUpdate = currentUserDocCopy.posts.find(
+        (eachPost) => eachPost.id === post.id
+      );
+      const indexOfUserEmail = postToUpdate.likedBy.indexOf(currentUserDoc.email);
+      if(indexOfUserEmail !== -1){
+        postToUpdate.likedBy.splice(indexOfUserEmail,1);
+      }
+     
+
+      return { ...currentUserDocCopy };
+    });
+  }
+
+  const removeFromLikedByArrayInAllUserDocs = () =>{
+    setAllUserDocs((prevState) => {
+      const allUserDocsCopy = [...prevState];
+      const docToModify = allUserDocsCopy.find(
+        (doc) => doc.email === post.email
+      );
+      const postToModify = docToModify.posts.find(
+        (eachPost) => eachPost.id === post.id
+      );
+      // if (postToModify.likedBy.indexOf(currentUserDoc.email) === -1) {
+      //   postToModify.likedBy.push(currentUserDoc?.email);
+      //   addToLikedByArrayInFireStore({...docToModify});
+      //   // postToModify.likedBy.push("hello sir");
+
+      // }
+      const indexOfCurrentUserEmail = postToModify?.likedBy?.indexOf(currentUserDoc.email);
+      if(indexOfCurrentUserEmail !== -1){
+postToModify.likedBy.splice(indexOfCurrentUserEmail,1);
+removeFromLikedByArrayInFireStore({...docToModify})
+      }
+      return [...allUserDocsCopy];
+    });
+  }
 
   const removeFromLikedPostsArray = () => {
     removeFromLikedPostsArrayInCurrentUserDoc();
     removeFromLikedPostsArrayInAllUserDocs();
+    if (post.email === currentUserDoc.email) {
+      removeFromLikedByArrayInCurrentUserDoc();
+    }
+    removeFromLikedByArrayInAllUserDocs();
   };
 
   const handleLike = () => {
@@ -286,38 +390,42 @@ const Post2 = ({ post }) => {
 
   const removePostFromCurrentUserDocs = () => {
     console.log(currentUserDoc?.posts?.length);
-   
-    setCurrentUserDoc(prevState => {
-      const currentUserDocsCopy = {...prevState};
-      const postToDelete = currentUserDocsCopy?.posts?.find(eachPost => eachPost.id === post.id);
-      const indexOfPostToDelete = currentUserDocsCopy.posts.indexOf(postToDelete);
-      if(indexOfPostToDelete !== -1){
-        currentUserDocsCopy.posts.splice(indexOfPostToDelete,1);
+
+    setCurrentUserDoc((prevState) => {
+      const currentUserDocsCopy = { ...prevState };
+      const postToDelete = currentUserDocsCopy?.posts?.find(
+        (eachPost) => eachPost.id === post.id
+      );
+      const indexOfPostToDelete =
+        currentUserDocsCopy.posts.indexOf(postToDelete);
+      if (indexOfPostToDelete !== -1) {
+        currentUserDocsCopy.posts.splice(indexOfPostToDelete, 1);
       }
-      return {...currentUserDocsCopy};
+      return { ...currentUserDocsCopy };
     });
     console.log(currentUserDoc?.posts?.length);
-    
   };
   const removePostFromAllUserDocs = () => {
-    
-
-    setAllUserDocs(prevState => {
+    setAllUserDocs((prevState) => {
       const allUserDocsCopy = [...allUserDocs];
-      const docToModify = allUserDocsCopy.find(doc => doc.username === currentUserDoc.username);
-      const postToDelete = docToModify.posts.find(eachPost => eachPost.id === post.id);
+      const docToModify = allUserDocsCopy.find(
+        (doc) => doc.username === currentUserDoc.username
+      );
+      const postToDelete = docToModify.posts.find(
+        (eachPost) => eachPost.id === post.id
+      );
       const indexOfPostToDelete = docToModify.posts.indexOf(postToDelete);
-      if(indexOfPostToDelete !== -1){
-        docToModify.posts.splice(indexOfPostToDelete,1);
+      if (indexOfPostToDelete !== -1) {
+        docToModify.posts.splice(indexOfPostToDelete, 1);
       }
       return [...allUserDocs];
-    })
+    });
   };
   const removePostFromFirestore = async () => {
     const currentUserDocRef = doc(db, "users", currentUserDoc.email);
 
     await updateDoc(currentUserDocRef, {
-      posts: arrayRemove(post)
+      posts: arrayRemove(post),
     });
   };
 
@@ -330,96 +438,103 @@ const Post2 = ({ post }) => {
   const handleCopyImageUrl = () => {
     navigator.clipboard.writeText(post.imageUrl);
     handleShareButtonSnackBarOpen();
-  }
+  };
 
   return (
     <>
-    <Card className={classes.root}>
-      <CardHeader
-        avatar={checkIfUserHasAvatar()}
-        action={
-          <>
-            {post.email === currentUserDoc.email && (
-              <IconButton onClick={handlePostDelete}>
-                <DeleteForeverIcon />
+      <Card className={classes.root}>
+        <CardHeader
+          avatar={checkIfUserHasAvatar()}
+          action={
+            <>
+              {post.email === currentUserDoc.email && (
+                <IconButton onClick={handlePostDelete}>
+                  <DeleteForeverIcon />
+                </IconButton>
+              )}
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
               </IconButton>
-            )}
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          </>
-        }
-        title={
-          <Typography
-            dangerouslySetInnerHTML={{
-              __html:
-                post?.username === currentUserDoc.username
-                  ? "You"
-                  : post.username,
-            }}
-            variant="h6"
-            component={Link}
-            to={`/profile/${post.username}`}
-            className={classes.postUsername}
+            </>
+          }
+          title={
+            <Typography
+              dangerouslySetInnerHTML={{
+                __html:
+                  post?.username === currentUserDoc.username
+                    ? "You"
+                    : post.username,
+              }}
+              variant="h6"
+              component={Link}
+              to={`/profile/${post.username}`}
+              className={classes.postUsername}
+            />
+          }
+          // subheader={`${post.date.toDate().getDate()}/${
+          //   post.date.toDate().getMonth() + 1
+          // }/${post.date.toDate().getFullYear()}`}
+          subheader={dateCustomizer(post.date)}
+        />
+        {post.imageUrl && (
+          <CardMedia
+            className={classes.media}
+            image={post.imageUrl}
+            title={post.username}
           />
-        }
-        // subheader={`${post.date.toDate().getDate()}/${
-        //   post.date.toDate().getMonth() + 1
-        // }/${post.date.toDate().getFullYear()}`}
-        subheader={dateCustomizer(post.date)}
-      />
-      {post.imageUrl && 
-      <CardMedia
-        className={classes.media}
-        image={post.imageUrl}
-        title={post.username}
-      />
-}
+        )}
 
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {post.text}
-        </Typography>
-      </CardContent>
-      <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          {post.text}
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          {checkIfPostInLikedPosts()}
-        </IconButton>
-        <IconButton aria-label="add to saved">
-          {checkIfPostInSavedPosts()}
-        </IconButton>
-        {post.imageUrl && 
-        (<IconButton aria-label="share" onClick={handleCopyImageUrl}>
-          <ShareIcon />
-        </IconButton>)}
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography variant="body2">{post.text}</Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {post.text}
+          </Typography>
         </CardContent>
-      </Collapse>
-    </Card>
-    <Snackbar open={openUrlCopiedSnackBar} autoHideDuration={1000} onClose={handleShareButtonSnackBarClose}>
-    <Alert onClose={handleShareButtonSnackBarClose} severity="success">
-      Image URL copied!
-    </Alert>
-  </Snackbar>
-  </>
+
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {post.likedBy.length} likes
+          </Typography>
+        </CardContent>
+
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            {checkIfPostInLikedPosts()}
+          </IconButton>
+          <IconButton aria-label="add to saved">
+            {checkIfPostInSavedPosts()}
+          </IconButton>
+          {post.imageUrl && (
+            <IconButton aria-label="share" onClick={handleCopyImageUrl}>
+              <ShareIcon />
+            </IconButton>
+          )}
+          <IconButton
+            className={clsx(classes.expand, {
+              [classes.expandOpen]: expanded,
+            })}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography variant="body2">{post.text}</Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+      <Snackbar
+        open={openUrlCopiedSnackBar}
+        autoHideDuration={1000}
+        onClose={handleShareButtonSnackBarClose}
+      >
+        <Alert onClose={handleShareButtonSnackBarClose} severity="success">
+          Image URL copied!
+        </Alert>
+      </Snackbar>
+    </>
   );
 };
 
