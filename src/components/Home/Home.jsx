91421a0115c6dae23fac634a67d9385ec65d1b54
chaitanya7666 <chaitu.y25@gmail.com>
2021-11-17 12:56:42 +0530

    Added : go to top button in Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,13 +4,12 @@ import Wassup from "../Wassup/Wassup";
 import NavBar from "../NavBar/NavBar";
 import NavBar2 from "../NavBar2/NavBar2";
 import AllPosts2 from "../AllPosts2/AllPosts2";
-
-
-
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import { db } from "../../firebase";
 import { doc, getDoc } from "firebase/firestore";
-import { Box } from "@material-ui/core";
+import { Box, IconButton, Tooltip } from "@material-ui/core";
 import { CurrentUserDetailsContext } from '../../contexts/CurrentUserDetailsContext';
+import useStyles from "./styles";
 
 const Home = ({handleAllPostsUpdateDeleteOptimistically,handleDummy,allPosts}) => {
   const { currentUser } = useAuth();
@@ -18,7 +17,11 @@ const Home = ({handleAllPostsUpdateDeleteOptimistically,handleDummy,allPosts}) =
     CurrentUserDetailsContext
   );
 
+  const classes = useStyles();
 
+  const handleGoToTop = () => {
+    window.scrollTo(0, 0);
+  };
     
 
 
@@ -30,6 +33,14 @@ const Home = ({handleAllPostsUpdateDeleteOptimistically,handleDummy,allPosts}) =
       <NavBar2/>
       <Wassup currentUser={currentUser} handleAllPostsUpdateDeleteOptimistically={handleAllPostsUpdateDeleteOptimistically} handleDummy={handleDummy} allPosts={allPosts}/>
       <AllPosts2/>
+      
+      <Box className={classes.goToTopButtonBox}>
+        <Tooltip title="Go to top">
+          <IconButton onClick={handleGoToTop}>
+            <ArrowUpwardIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
     </div>
   );
 };
