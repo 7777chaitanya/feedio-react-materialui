e14498531f03fe4d4b1a5d13a2092affc0775fdd
Chaitanya7666 <chaitu.y25@gmail.com>
2021-11-17 22:16:36 +0530

    Added : user suggestion redirects to user profile

--- src/components/Home/UserSuggestion/EachUserDetail/EachUserDetail.jsx
+++ src/components/Home/UserSuggestion/EachUserDetail/EachUserDetail.jsx
@@ -4,13 +4,19 @@ import { Typography } from "@material-ui/core";
 import { Divider } from "@material-ui/core";
 import { ListItem, ListItemAvatar, ListItemText } from "@material-ui/core";
 import useStyles from "./styles";
+import {useHistory} from "react-router-dom";
 
 const EachUserDetail = ({avatarUrl, bio, userName}) => {
   const classes = useStyles();
+  const history = useHistory()
+
+  const handleEnterUserProfile = () => {
+    history.push(`/profile/${userName}`);
+  }
 
   return (
     <>
-      <ListItem alignItems="flex-start">
+      <ListItem alignItems="flex-start" button={true} onClick={handleEnterUserProfile}>
         <ListItemAvatar>
           <Avatar
             alt="Remy Sharp"
