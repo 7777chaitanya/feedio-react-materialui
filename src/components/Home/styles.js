33584e945ec24870397fc5334297c3e47965eaf6
Chaitanya7666 <chaitu.y25@gmail.com>
2021-11-17 13:24:30 +0530

    styles

--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -8,7 +8,7 @@ const  useStyles = makeStyles({
         right : "2rem",
         border : "1px solid gray",
         borderRadius : "50px",
-        backgroundColor : "rgb(147,104,247)"
+        backgroundColor : "rgb(107,187,117)"
     }
 })
 
