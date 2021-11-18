b6ab58545b0f4ed4fb19abccfdf0ef74afa1c703
Chaitanya7666 <chaitu.y25@gmail.com>
2021-11-18 21:52:16 +0530

    styles : custom scroll bar

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
   MyPosts,
   AllPosts,
   Profile,
-  Footer
+  Footer,
 } from "./components";
 import "./index.css";
 import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
@@ -21,9 +21,10 @@ import PrivateRoute from "./components/PrivateRoute";
 import { CurrentUserDetailsProvider } from "./contexts/CurrentUserDetailsContext";
 import { AllUserDetailsProvider } from "./contexts/AllUserDetailsContext";
 import { ThemeProvider, createTheme } from "@material-ui/core";
-import {useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import "./App.css";
 import { ClickProvider } from "./contexts/ClickContext";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const App = () => {
   const [docs, setDocs] = useState([]);
@@ -46,13 +47,44 @@ const App = () => {
         main: "rgb(107,187,117)",
         dark: "rgb(18,16,14)",
       },
-      secondary : {
+      secondary: {
         // main : "#808000",
         // main: "rgb(232, 243, 236)",
         main: "rgb(232, 232, 232)",
 
-        dark : "rgb(0, 171, 107)"
-      }
+        dark: "rgb(0, 171, 107)",
+      },
+    },
+    overrides: {
+      MuiCssBaseline: {
+        "@global": {
+          body: {
+            scrollbarColor: "#6b6b6b",
+            "&::-webkit-scrollbar, & *::-webkit-scrollbar": {
+              backgroundColor: "lightgray",
+              width : "0.3rem"
+            },
+            "&::-webkit-scrollbar-thumb, & *::-webkit-scrollbar-thumb": {
+              borderRadius: 8,
+              backgroundColor: "#9dd2a4",
+              minHeight: 24,
+              border: "1px solid #2b2b2b",
+            },
+            "&::-webkit-scrollbar-thumb:focus, & *::-webkit-scrollbar-thumb:focus": {
+              backgroundColor: "#959595",
+            },
+            "&::-webkit-scrollbar-thumb:active, & *::-webkit-scrollbar-thumb:active": {
+              backgroundColor: "#959595",
+            },
+            "&::-webkit-scrollbar-thumb:hover, & *::-webkit-scrollbar-thumb:hover": {
+              backgroundColor: "#959595",
+            },
+            "&::-webkit-scrollbar-corner, & *::-webkit-scrollbar-corner": {
+              backgroundColor: "#2b2b2b",
+            },
+          },
+        },
+      },
     },
   });
 
@@ -61,14 +93,12 @@ const App = () => {
   };
 
   useEffect(async () => {
-    
     let allPosts = [];
     const querySnapshot = await getDocs(collection(db, "users"));
     querySnapshot.forEach((doc) => {
       allPosts.push(doc.data());
     });
     setAllPosts(allPosts);
-    
   }, []);
 
   const handleLike = (e) => {
@@ -92,6 +122,7 @@ const App = () => {
 
   return (
     <div className="mainBox">
+
       <Router>
         <ToastContainer
           position="top-right"
@@ -104,67 +135,70 @@ const App = () => {
           draggable
           pauseOnHover
         />
+
         <ThemeProvider theme={theme}>
-        <AuthProvider>
-          <CurrentUserDetailsProvider>
-            <AllUserDetailsProvider>
-              <ClickProvider>
-              <Switch>
-                <Route path="/login">
-                  <Login />
-                </Route>
-                <Route path="/signup">
-                  <Signup />
-                </Route>
-                <Route path="/forgot-password">
-                  <ForgotPassword />
-                </Route>
-                {/* <Route path="/my-posts" >
+        <CssBaseline />
+
+          <AuthProvider>
+            <CurrentUserDetailsProvider>
+              <AllUserDetailsProvider>
+                <ClickProvider>
+                  <Switch>
+                    <Route path="/login">
+                      <Login />
+                    </Route>
+                    <Route path="/signup">
+                      <Signup />
+                    </Route>
+                    <Route path="/forgot-password">
+                      <ForgotPassword />
+                    </Route>
+                    {/* <Route path="/my-posts" >
               <MyPosts handleLike={handleLike} like={like} allPosts={allPosts}/>
             </Route> */}
-                <Route path="/my-posts">
-                  <MyPosts
-                    handleLike={handleLike}
-                    like={like}
-                    allPosts={allPosts}
-                    handleAllPostsUpdateDeleteOptimistically={
-                      handleAllPostsUpdateDeleteOptimistically
-                    }
-                  />
-                </Route>
-                <Route path="/all-posts">
-                  <AllPosts
-                    handleLike={handleLike}
-                    like={like}
-                    allPosts={allPosts}
-                    handleAllPostsUpdateDeleteOptimistically={
-                      handleAllPostsUpdateDeleteOptimistically
-                    }
-                  />
-                </Route>
-                <Route
-                  path="/profile/:username"
-                  render={(props) => <Profile {...props} />}
-                />
-                {/* <Route path="/" exact>
+                    <Route path="/my-posts">
+                      <MyPosts
+                        handleLike={handleLike}
+                        like={like}
+                        allPosts={allPosts}
+                        handleAllPostsUpdateDeleteOptimistically={
+                          handleAllPostsUpdateDeleteOptimistically
+                        }
+                      />
+                    </Route>
+                    <Route path="/all-posts">
+                      <AllPosts
+                        handleLike={handleLike}
+                        like={like}
+                        allPosts={allPosts}
+                        handleAllPostsUpdateDeleteOptimistically={
+                          handleAllPostsUpdateDeleteOptimistically
+                        }
+                      />
+                    </Route>
+                    <Route
+                      path="/profile/:username"
+                      render={(props) => <Profile {...props} />}
+                    />
+                    {/* <Route path="/" exact>
               <Home />
             </Route> */}
-                <PrivateRoute
-                  component={Home}
-                  allPosts={allPosts}
-                  handleAllPostsUpdateDeleteOptimistically={
-                    handleAllPostsUpdateDeleteOptimistically
-                  }
-                  handleDummy={handleDummy}
-                  path="/"
-                  exact
-                />
-              </Switch>
-              <Footer />
-              </ClickProvider>
-            </AllUserDetailsProvider>
-          </CurrentUserDetailsProvider>
-        </AuthProvider>
+                    <PrivateRoute
+                      component={Home}
+                      allPosts={allPosts}
+                      handleAllPostsUpdateDeleteOptimistically={
+                        handleAllPostsUpdateDeleteOptimistically
+                      }
+                      handleDummy={handleDummy}
+                      path="/"
+                      exact
+                    />
+                  </Switch>
+                  <Footer />
+                </ClickProvider>
+              </AllUserDetailsProvider>
+            </CurrentUserDetailsProvider>
+          </AuthProvider>
         </ThemeProvider>
       </Router>
     </div>
