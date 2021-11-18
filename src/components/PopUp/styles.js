f4d943f026aeb07a253e113d421500969f23625a
Chaitanya7666 <chaitu.y25@gmail.com>
2021-11-18 11:41:31 +0530

    styles : scroll bar to search pop up

--- src/components/PopUp/styles.js
+++ src/components/PopUp/styles.js
@@ -1,21 +1,22 @@
 import { makeStyles } from "@material-ui/core";
 
-const useStyles = makeStyles(theme=>( {
-    root : {
-        position : "absolute",
-        
-        zIndex : 100,
-        backgroundColor : "rgb(235,235,235)",
-        left : "15vw",
-        top : "12vh",
-        minWidth : "20vw",
-        paddingLeft : "1vw",
-        paddingRight : "1vw",
-        paddingBottom : "1vw",
-        [theme.breakpoints.down('sm')]: {
-            minWidth : "60vw",          }
-    }
-
-}))
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "absolute",
+    zIndex: 100,
+    backgroundColor: "rgb(235,235,235)",
+    left: "15vw",
+    top: "12vh",
+    minWidth: "20vw",
+    paddingLeft: "1vw",
+    paddingRight: "1vw",
+    paddingBottom: "1vw",
+    [theme.breakpoints.down("sm")]: {
+      minWidth: "60vw",
+    },
+    maxHeight: "60vh",
+    overflow: "auto",
+  },
+}));
 
 export default useStyles;
