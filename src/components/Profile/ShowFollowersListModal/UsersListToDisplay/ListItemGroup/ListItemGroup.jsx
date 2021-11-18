9bc683d864c2e4bb83115992f429c9b8e5de623e
Chaitanya7666 <chaitu.y25@gmail.com>
2021-11-18 17:10:21 +0530

    Added : followers and following modal

---
+++ src/components/Profile/ShowFollowersListModal/UsersListToDisplay/ListItemGroup/ListItemGroup.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import CommentIcon from '@material-ui/icons/Comment';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: 360,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function ListItemGroup({usersToShow}) {
+  const classes = useStyles();
+ 
+
+  return (
+    <List className={classes.root}>
+      {usersToShow.map((value) => {
+
+        return (
+          <ListItem key={value?.email} role={undefined} dense button
+        //    onClick={}
+           >
+            <ListItemAvatar>
+              <Avatar
+                alt={value?.email}
+                src={value?.avatarUrl}
+              />
+            </ListItemAvatar>
+            <ListItemText  primary={value?.username} />
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="comments">
+                <CommentIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+}
